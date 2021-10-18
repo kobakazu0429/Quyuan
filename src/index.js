13db32953b498f7ad0966cd19c88ee06f4fb99f6
kobakazu0429 <kobakazu10@gmail.com>
2021-10-18 10:27:06 +0900

    feat:panorama

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const templateExtractor = require("./templateExtractor.js")
 const createSwiper = require("./swiper.js")
+const openPanorama = require("./panorama")
 
 console.log("hello world")
 
 module.exports = {
   templateExtractor,
-  createSwiper
+  createSwiper,
+  openPanorama
 };
