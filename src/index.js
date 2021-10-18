f86a1acb7cfc368a709162e40a909b1e4e83e719
kobakazu0429 <kobakazu10@gmail.com>
2021-10-18 10:45:06 +0900

    remove console.log

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const templateExtractor = require("./templateExtractor.js")
 const createSwiper = require("./swiper.js")
 const openPanorama = require("./panorama")
 
-console.log("hello world")
-
 module.exports = {
   templateExtractor,
   createSwiper,
